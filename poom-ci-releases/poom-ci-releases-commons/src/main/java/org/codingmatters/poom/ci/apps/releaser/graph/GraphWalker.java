fe756ec2cd101a000a391c7903a57fa6a9e0b495
nel <nel@flexio.fr>
2024-01-23 12:55:56 +0100

    subtasks termination propper handling

--- poom-ci-releases/poom-ci-releases-commons/src/main/java/org/codingmatters/poom/ci/apps/releaser/graph/GraphWalker.java
+++ poom-ci-releases/poom-ci-releases-commons/src/main/java/org/codingmatters/poom/ci/apps/releaser/graph/GraphWalker.java
@@ -7,10 +7,7 @@ import org.codingmatters.poom.ci.apps.releaser.task.ReleaseTaskResult;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class GraphWalker implements Callable<GraphWalkResult> {
     public interface WalkerTaskProvider {
@@ -54,13 +51,16 @@ public class GraphWalker implements Callable<GraphWalkResult> {
                 subtasks.add(this.pool.submit(new GraphWalker(new RepositoryGraphDescriptor(graph), this.propagationContext, this.walkerTaskProvider, this.pool)));
             }
         }
+        System.out.printf("graph has %s subtasks\n", subtasks.size());
+        this.waitFor(subtasks);
+        System.out.printf("graph %s subtasks done.\n", subtasks.size());
 
         boolean failures = false;
         StringBuilder failureMessages = new StringBuilder();
+        int i = 0;
         for (Future<GraphWalkResult> subtask : subtasks) {
             GraphWalkResult result = subtask.get();
-//            if(! result.exitStatus().equals(ReleaseTaskResult.ExitStatus.SUCCESS)) {
-            if(ReleaseTaskResult.ExitStatus.FAILURE.equals(result.exitStatus())) {
+            if(! result.exitStatus().equals(ReleaseTaskResult.ExitStatus.SUCCESS)) {
                 failures = true;
                 failureMessages.append("- ").append(result.message()).append("\n");
             }
@@ -69,6 +69,26 @@ public class GraphWalker implements Callable<GraphWalkResult> {
             return Optional.of(new GraphWalkResult(ReleaseTaskResult.ExitStatus.FAILURE, failureMessages.toString()));
         } else {
             return Optional.empty();
+        }
+    }
+
+    private void waitFor(List<Future<GraphWalkResult>> subtasks) {
+        List<Future<GraphWalkResult>>  remaining = new LinkedList<>(subtasks);
+        while(! remaining.isEmpty()) {
+            List<Future<GraphWalkResult>> done = new LinkedList<>();
+            for (Future<GraphWalkResult> future : remaining) {
+                if(future.isDone()) {
+                    done.add(future);
+                }
+            }
+            remaining.removeAll(done);
+            if(! remaining.isEmpty()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 }
